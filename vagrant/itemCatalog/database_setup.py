8783e0b61413d8b6ed24c567dad02165194c9c84
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-14 23:02:57 -0800

    Setup for SQLAlchemy

--- vagrant/itemCatalog/database_setup.py
+++ vagrant/itemCatalog/database_setup.py
@@ -1,3 +1,26 @@
 import sys
 
 from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationships
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Restaurant(Base):
+    pass
+
+
+class MenuItem(Base):
+    pass
+
+
+# EOF statements
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
