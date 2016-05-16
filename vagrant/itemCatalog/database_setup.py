5404e2fcc54517d0a700c56d2c44a5ebeeb4728d
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-15 23:11:10 -0800

    Generates and initiates the new ORM db

--- vagrant/itemCatalog/database_setup.py
+++ vagrant/itemCatalog/database_setup.py
@@ -1,10 +1,10 @@
 import sys
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
 
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy.orm import relationships
+from sqlalchemy.orm import relationship
 
 from sqlalchemy import create_engine
 
@@ -12,11 +12,21 @@ Base = declarative_base()
 
 
 class Restaurant(Base):
-    pass
+    __tablename__ = 'restaurant'
+    name = Column(Text, nullable=False)
+    id = Column(Integer, primary_key=True)
 
 
 class MenuItem(Base):
-    pass
+    __tablename__ = 'menu_item'
+    name = Column(Text, nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(Text)
+    description = Column(Text)
+    # TODO: Convert to a better type once its switched to postgreSQL
+    price = Column(String(9))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
 
 
 # EOF statements
