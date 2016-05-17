8ee563b459129969dc3d0f76300404dffd5f3344
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-16 21:49:29 -0800

    Corrected enum type

--- vagrant/itemCatalog/tutorial_shelter.py
+++ vagrant/itemCatalog/tutorial_shelter.py
@@ -1,42 +1,36 @@
 from sqlalchemy import Column, ForeignKey, Integer, Text, Date, Enum, Numeric
-
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import relationship
-
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 
-class Gender(Enum):
-    male = 'male'
-    female = 'female'
-
-
 class Shelter(Base):
     __tablename__ = 'shelter'
+    id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False)
     address = Column(Text, nullable=False)
     city = Column(Text, nullable=False)
     state = Column(Text, nullable=False)
     zipCode = Column(Integer, nullable=False)
     website = Column(Text)
-    id = Column(Integer, primary_key=True)
 
 
 class Puppy(Base):
     __tablename__ = 'puppy'
+    id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False)
-    birth_date = Column(Date)
-    gender = Column(Gender)
+    dateOfBirth = Column(Date)
+    gender = Column(Enum('male', 'female'))
     weight = Column(Numeric)
+    picture = Column(Text)
     shelter_id = Column(Integer, ForeignKey('shelter.id'))
     shelter = relationship(Shelter)
 
 
 # EOF statements
 
-engine = create_engine('sqlite:///shelter.db')
+engine = create_engine('sqlite:///puppyshelter.db')
 
 Base.metadata.create_all(engine)
