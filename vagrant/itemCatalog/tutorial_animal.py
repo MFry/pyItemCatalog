0869307ab61b3d012d8f7f201865af8d23484d58
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-16 21:18:33 -0800

    Created table definitions for Shelter and Puppy tables.

--- vagrant/itemCatalog/tutorial_animal.py
+++ vagrant/itemCatalog/tutorial_animal.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, ForeignKey, Integer, Text, Date, Enum, Numeric
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,7 +9,13 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class Gender(Enum):
+    male = 'male'
+    female = 'female'
+
+
 class Shelter(Base):
+    __tablename__ = 'shelter'
     name = Column(Text, nullable=False)
     address = Column(Text, nullable=False)
     city = Column(Text, nullable=False)
@@ -17,6 +23,16 @@ class Shelter(Base):
     zipCode = Column(Integer, nullable=False)
     website = Column(Text)
     id = Column(Integer, primary_key=True)
+
+
+class Puppy(Base):
+    __tablename__ = 'puppy'
+    name = Column(Text, nullable=False)
+    birth_date = Column(Date)
+    gender = Column(Gender)
+    weight = Column(Numeric)
+    shelter_id = Column(Integer, ForeignKey('shelter.id'))
+    shelter = relationship(Shelter)
 
 
 # EOF statements
