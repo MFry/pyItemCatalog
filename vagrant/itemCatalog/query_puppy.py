1bd3dc1bf2d5429bd837bc97476919588a9ef987
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-17 22:47:06 -0800

    Testing queries

--- vagrant/itemCatalog/query_puppy.py
+++ vagrant/itemCatalog/query_puppy.py
@@ -1,8 +1,9 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///puppyshelter.db')
 from tutorial_shelter import Base, Shelter, Puppy
+from datetime import datetime, timedelta
 
 Base.metadata.bind = engine
 
@@ -11,6 +12,18 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 puppies = session.query(Puppy).order_by(Puppy.name.asc())
-
 for pup in puppies:
     print(pup.name)
+print()
+young_puppies = session.query(Puppy).filter(datetime.now() - timedelta(weeks=6*4) <= Puppy.dateOfBirth).order_by(Puppy.dateOfBirth.asc())
+for pup in young_puppies:
+    print(pup.name, pup.dateOfBirth)
+print()
+weight_puppies = session.query(Puppy).order_by(Puppy.weight.asc())
+for pup in weight_puppies:
+    print(pup.name, pup.weight)
+print()
+shelters = session.query(func.count(Puppy.shelter_id), Puppy.shelter_id).group_by(Puppy.shelter_id).all()
+print(shelters)
+for shelter in shelters:
+    print(shelter.shelter_id)
