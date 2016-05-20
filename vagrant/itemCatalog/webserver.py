897125dc0c1c53f4a3914e8f4b156a91d390cc1a
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-19 20:48:16 -0800

    Fixed function typo and encoded the string to get wfile.write working with python3

--- vagrant/itemCatalog/webserver.py
+++ vagrant/itemCatalog/webserver.py
@@ -1,17 +1,18 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-class webserverHandler(BaseHTTPRequestHandler):
+class WebserverHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         try:
-            if self.path.endwith("/hello"):
+            if self.path.endswith("/hello"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
                 output = ""
                 output += "<html><body>Hello!</body></html>"
-                self.wfile.write(output)
+                self.wfile.write(output.encode('utf-8'))
                 print(output)
 
         except IOError:
@@ -21,7 +22,7 @@ class webserverHandler(BaseHTTPRequestHandler):
 def main():
     try:
         port = 8080
-        server = HTTPServer(('', port), webserverHandler)
+        server = HTTPServer(('', port), WebserverHandler)
         print("Web server is running on port {}".format(port))
         server.serve_forever()
 
@@ -29,7 +30,8 @@ def main():
         print("^C entered, stopping web server...")
 
     finally:
-        server.socket.close()
+        if server:
+            server.socket.close()
 
 
 if __name__ == '__main__':
