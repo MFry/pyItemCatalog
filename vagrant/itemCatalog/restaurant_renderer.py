7ba798d1789175bea5782ba81db9b527c27fa181
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-23 18:27:00 -0800

    HTML renderer specific for rendering restaurant information

---
+++ vagrant/itemCatalog/restaurant_renderer.py
@@ -0,0 +1,12 @@
+from HTML_renderer import HTMLRenderer
+
+
+class RestaurantRenderer(HTMLRenderer):
+    def restaurant(self, name):
+        content = "{name}".format(name=name)
+        return self.render_simple_block(content)
+
+    def render_restaurants(self, restaurants):
+        content = ''
+        for restaurant in restaurants:
+            content += self.restaurant(restaurant) + '\n'
