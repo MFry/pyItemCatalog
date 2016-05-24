63bc1bee4df48672910c46135086da0613e4866c
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-24 11:04:26 -0800

    Objective2: Of creating edit and delete links for each restaurant

--- vagrant/itemCatalog/restaurant_renderer.py
+++ vagrant/itemCatalog/restaurant_renderer.py
@@ -3,7 +3,9 @@ from HTML_renderer import HTMLRenderer
 
 class RestaurantRenderer(HTMLRenderer):
     def restaurant(self, name):
-        content = "{name}".format(name=name)
+        content = "{name}<br>\n" \
+                  "<a href=#>edit</a><br>\n" \
+                  "<a href=#>delete</a>\n".format(name=name)
         return self.render_simple_block(content)
 
     def render_restaurants(self, restaurants):
