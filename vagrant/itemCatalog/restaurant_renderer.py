956fa505f2270502d4acef6f77562bf154f0a47a
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-24 17:50:36 -0800

    Changed the way default restaurants are rendered for readability

--- vagrant/itemCatalog/restaurant_renderer.py
+++ vagrant/itemCatalog/restaurant_renderer.py
@@ -2,14 +2,14 @@ from HTML_renderer import HTMLRenderer
 
 
 class RestaurantRenderer(HTMLRenderer):
-    def restaurant(self, name):
-        content = "{name}<br>\n" \
-                  "<a href='#'>Edit</a><br>\n" \
-                  "<a href='#'>Delete</a>\n".format(name=name)
+    def restaurant(self, name, link):
+        content = "<span style='font-size:1.25em;'>{name}</span><br>\n" \
+                  "<a href='{edit}/edit'>Edit</a><br>\n" \
+                  "<a href='{delete}/delete'>Delete</a>\n".format(name=name, edit=link, delete=link)
         return self.render_simple_block(content)
 
     def render_restaurants(self, restaurants):
         content = ''
         for restaurant in restaurants:
-            content += self.restaurant(restaurant) + '\n'
+            content += self.restaurant(restaurant.name, restaurant.id) + '\n'
         return content
