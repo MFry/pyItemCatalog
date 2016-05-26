c9ae08b11ec7674dc6a0a4960c43d115d0ef8201
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-25 20:16:44 -0800

    Formatted for PEP8

--- vagrant/itemCatalog/project.py
+++ vagrant/itemCatalog/project.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, redirect
-
-app = Flask(__name__)
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 DBSession = sessionmaker(bind=engine)
