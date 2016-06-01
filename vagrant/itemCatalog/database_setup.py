b0b4e1cb32ce8b2e1def047c9c0746c132fe2dac
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-06-01 15:49:50 -0800

    Added menu list definition

--- vagrant/itemCatalog/database_setup.py
+++ vagrant/itemCatalog/database_setup.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Text, Integer
+from sqlalchemy import Column, Text, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -11,6 +12,17 @@ class Restaurant(Base):
     __tablename__ = 'restaurant'
     name = Column(Text, nullable=False)
     id = Column(Integer, primary_key=True)
+
+
+class Menu(Base):
+    __tablename__ = 'menu'
+    name = Column(Text, nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(Text)
+    description = Column(Text)
+    price = Column(String(7))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
 
 
 engine = create_engine(connection_str)
