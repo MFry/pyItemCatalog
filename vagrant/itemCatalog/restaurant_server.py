994eee3f4091a86193209eac6951314ce8642adc
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-05-31 18:15:01 -0800

    Implemented function stubs for all CRUD functionality

--- vagrant/itemCatalog/restaurant_server.py
+++ vagrant/itemCatalog/restaurant_server.py
@@ -9,9 +9,33 @@ def home():
     return render_template('restaurants_list.html')
 
 
+def new_restaurant():
+    return None
+
+
+def edit_restaurant(restaurant_id):
+    return None
+
+
+def delete_restaurant(restaurant_id):
+    return None
+
+
 @app.route('/restaurants/<int:restaurant_id>/menu/')
 def menu(restaurant_id):
     return render_template('menu_list.html')
+
+
+def new_menu(restaurant_id):
+    return None
+
+
+def update_menu(restaurant_id, menu_id):
+    return None
+
+
+def delete_menu_item(restaurant_id, menu_id):
+    return None
 
 
 if __name__ == '__main__':
