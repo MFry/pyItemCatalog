85df1dbb33b959924197af6326a0effe0edf5263
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-06-01 13:25:45 -0800

    Fixed connection string bug

--- vagrant/itemCatalog/database_setup.py
+++ vagrant/itemCatalog/database_setup.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-connection_str = 'postgresql://vagrant:vagrant@locahost/restaurant_menu'
+connection_str = 'postgresql://vagrant:vagrant@localhost/restaurant_menu'
 
 
 class Restaurant(Base):
