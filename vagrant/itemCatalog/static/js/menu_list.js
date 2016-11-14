c1b329639d07faf8248865a092a37187ea7d3da2
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-11-13 23:05:36 -0900

    Feat: Created a new javascript file for react.js and added basic support for restaurant menu items

---
+++ vagrant/itemCatalog/static/js/menu_list.js
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+class MenuList extends React.Component {
+    render() {
+        return ('');
+    }
+}
+
+function restaurantItem(prop) {
+    return (
+        <div class="row">
+            <div class="col-md-6">{prop.name}
+            </div>
+        </div>
+    );
+}
