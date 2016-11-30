2376bc6aa2119255e6e3cd364f83f7b82d91e1e6
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-11-29 22:20:08 -0900

    Chore: Added webpack config file

---
+++ vagrant/webpack.config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by michal frystacky on 11/27/16.
+ */
+
+
+// @Ref: https://blog.risingstack.com/the-react-way-getting-started-tutorial/
+// @Ref: https://www.codementor.io/tamizhvendan/tutorials/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack-du107r9zr
+// @Ref: http://survivejs.com/webpack/advanced-techniques/configuring-react/
