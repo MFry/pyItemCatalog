330fc177e01ff1d6ce3824d6b94a6ed29ddc7983
Michal Frystacky <Michal.Frystacky@alaska.gov>
2016-11-30 18:42:37 -0900

    Feat: Sample code for working webpack example.

--- vagrant/webpack.config.js
+++ vagrant/webpack.config.js
@@ -6,3 +6,24 @@
 // @Ref: https://blog.risingstack.com/the-react-way-getting-started-tutorial/
 // @Ref: https://www.codementor.io/tamizhvendan/tutorials/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack-du107r9zr
 // @Ref: http://survivejs.com/webpack/advanced-techniques/configuring-react/
+
+
+module.exports = {
+    entry: `${__dirname}/src/app.js`,
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
