ae72c9c9c7d3188939e82da8bf224206bf828f6f
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-12-05 18:45:48 -0900

    Fix: Added the forgotten install of npm

--- vagrant/npm_provisions.sh
+++ vagrant/npm_provisions.sh
@@ -1,3 +1,4 @@
 # Thanks to tecadminm @Ref: http://tecadmin.net/install-latest-nodejs-npm-on-ubuntu/#
 sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs
