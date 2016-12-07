bca4cabb4ab472f8f016e111194465d676569bd8
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-12-06 15:39:12 -0900

    Chore: Added additional steps to install npm and npm update checker

--- vagrant/npm_provisions.sh
+++ vagrant/npm_provisions.sh
@@ -2,3 +2,5 @@
 sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs
+sudo npm install -g npm
+sudo npm install -g npm-check-updates
