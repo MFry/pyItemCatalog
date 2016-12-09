10e0931afe132b24772f6854964e36c10308653f
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-12-08 18:31:15 -0900

    Chore: Improved the regex for loaders test

--- vagrant/itemCatalog/webpack.config.js
+++ vagrant/itemCatalog/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.jsx$/,
+                test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query: {
