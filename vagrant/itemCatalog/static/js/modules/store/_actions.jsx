5b5a6d53a3755623ab84236b026e15096cd39d8d
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-12-19 23:40:41 -0900

    Fix: Encapsulating redux middleware dispatch for ajax requests

--- vagrant/itemCatalog/static/js/modules/store/_actions.jsx
+++ vagrant/itemCatalog/static/js/modules/store/_actions.jsx
@@ -1,10 +1,22 @@
 /**
  * Created by michalfrystacky on 12/16/16.
  */
-
-export const LOAD_CONTENT = 'LOAD_CONTENT';
+import * as types from './types';
 
 //Action Creator
 export function loadContent(content) {
-    return {type: LOAD_CONTENT, content}
+    return {type: types.LOAD_CONTENT, content}
+}
+
+//Dispatch Creator
+export function requestRestaurants() {
+    return {
+        [types.API_REQUEST_RESTAURANTS]: {
+            method: 'get',
+            path: '/restaurants/json/',
+            sendingType: types.API_REQUEST_RESTAURANTS,
+            successType: types.API_REQUEST_SUCCESS,
+            failureType: types.API_REQUEST_FAILURE
+        }
+    }
 }
---
+++ vagrant/itemCatalog/static/js/modules/store/types.jsx
@@ -0,0 +1,11 @@
+/**
+ * Created by michalfrystacky on 12/18/16.
+ */
+
+export const LOAD_CONTENT = 'LOAD_CONTENT';
+
+export const API_REQUEST_RESTAURANTS = 'API_REQUEST_RESTAURANTS';
+
+export const API_REQUEST_SUCCESS = 'API_REQUEST_SUCCESS';
+
+export const API_REQUEST_FAILURE = 'API_REQUEST_FAILURE';
