62a856409fdc5fa06f6d08b6008b0bf159cfbe48
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-01-04 18:05:16 -0900

    Refactor: Moved content.jsx out of the store

---
+++ vagrant/itemCatalog/static/js/modules/content.jsx
@@ -0,0 +1,33 @@
+/**
+ * Created by michalfrystacky on 12/28/16.
+ */
+
+import {RestaurantListContainer} from './restaurantList';
+
+
+export default body_content = RestaurantListContainer;
+/*
+ export default class BodyView extends React.Component {
+
+ constructor() {
+ super();
+ this.state = {
+ renderClass: RestaurantListContainer,
+ }
+ }
+
+ renderBody(newContainer) {
+ this.setState({
+ renderClass: newcontainer,
+ });
+ }
+
+ render() {
+ return (
+ <div className="container">
+ <this.state.renderClass />
+ </div>
+ );
+ }
+ }
+ */
