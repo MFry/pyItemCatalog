64eb6ab6f7e4f798a9a930cb1e4afe496cf30785
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-01-15 23:19:21 -0900

    Feat: Added react-redux to the main component so that the store is visible

--- vagrant/itemCatalog/static/js/modules/main.jsx
+++ vagrant/itemCatalog/static/js/modules/main.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {provider} from 'react-redux';
 import {content} from './content';
 import configureStore from './store/store';
 import {RestaurantListContainer} from './restaurantList';
 
 const store = configureStore();
 //Todo: http://redux.js.org/docs/basics/UsageWithReact.html#passing-the-store
-render(<content value={store.getState()}/>, document.getElementById('reactTest'));
-
-//import {configureStore} from './store/configureStore';
-
-/*const store = configureStore();
-
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+    <provider store={store}>
+        <content />
+    </provider>,
+    document.getElementById('reactTest')
 );
- */
