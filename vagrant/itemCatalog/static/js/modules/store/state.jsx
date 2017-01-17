4c5650f80494e6a59c3c59759bfd1bfd95e6a97c
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-01-16 23:51:46 -0900

    Feat: Created composite reducers with a filter reducer to render

--- vagrant/itemCatalog/static/js/modules/store/state.jsx
+++ vagrant/itemCatalog/static/js/modules/store/state.jsx
@@ -3,7 +3,8 @@ import * as actions from './actions';
 import expect from 'expect';
 
 //Reducer for content updates
-const content_reducer = (state = {}, action) => {
+
+const contentReducer = (state = {}, action) => {
     switch (action.type) {
         case actions.LOAD_CONTENT:
             return {content: action.content};
@@ -13,16 +14,39 @@ const content_reducer = (state = {}, action) => {
     }
 };
 
-//expect(content_reducer({}, 'DEFAULT')).toEqual({});
-expect(true).toEqual(true);
+const visibilityFilter = (state = 'SHOW_RESTAURANTS',
+                          action) => {
+    switch (action.type) {
+        case 'SHOW_MENU':
+            return state;
+        default:
+            return state;
+    }
+};
+
+const content_view = (state = {}, action) => {
+    return {
+        content: contentReducer(
+            state,
+            action.content
+        ),
+        visibilityFilter: visibilityFilter(
+            state.visibilityFilter,
+            action
+        )
+    }
+};
+
+
+expect(contentReducer({}, 'DEFAULT')).toEqual({});
 
 const contentBefore = {type: 'LOAD_CONTENT', content: 'Body information'};
 const contentAfter = {content: 'Body information'};
 
-expect(content_reducer({}, contentBefore)).toEqual(contentAfter);
+expect(contentReducer({}, contentBefore)).toEqual(contentAfter);
 
 deepfreeze(contentBefore);
 
 export const reducers = combineReducers({
-    content: content_reducer
+    content: contentReducer
 });
