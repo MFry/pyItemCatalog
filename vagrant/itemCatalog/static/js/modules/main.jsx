3a56d52a905e6865192c3a1fe1e717082555ff3d
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-01-19 23:28:31 -0900

    Feat: Added a custom render method to subsrcibe to the store

--- vagrant/itemCatalog/static/js/modules/main.jsx
+++ vagrant/itemCatalog/static/js/modules/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import {provider} from 'react-redux';
 import {content} from './content';
 import configureStore from './store/store';
@@ -7,9 +7,15 @@ import {RestaurantListContainer} from './restaurantList';
 
 const store = configureStore();
 //Todo: http://redux.js.org/docs/basics/UsageWithReact.html#passing-the-store
-render(
-    <provider store={store}>
-        <content />
-    </provider>,
-    document.getElementById('reactTest')
-);
+const render = () => {
+    ReactDOM.render(
+        <provider store={store}>
+            <content />
+        </provider>,
+        document.getElementById('reactTest')
+    );
+};
+
+
+store.subscribe(render);
+render();
