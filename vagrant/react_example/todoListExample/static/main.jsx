5f4ab9ac6321258c897a32b294087392e0d5fe77
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-01-28 15:22:55 -0900

    Feat: Added visibility filter reducer

--- vagrant/react_example/todoListExample/static/main.jsx
+++ vagrant/react_example/todoListExample/static/main.jsx
@@ -41,6 +41,30 @@ const todos = (state = [], action) => {
     }
 };
 
+const visibilityFilter = (state = 'SHOW_ALL',
+                          action) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+
+const todoApp = (state = {}, action) => {
+    return {
+        todos: todo(
+            state.todo,
+            action
+        ),
+        visibilityFilter: visibilityFilter(
+            state.visibilityFilter,
+            action
+        )
+    }
+};
+
+
 const testAddTodo = () => {
     const stateBefore = [];
     const action = {
