59f8abff2f05662eeabb054991e9b3b1bd9379be
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-01-28 23:18:23 -0900

    Feat: Calling the new top level reducer and added an additional test.

--- vagrant/react_example/todoListExample/static/main.jsx
+++ vagrant/react_example/todoListExample/static/main.jsx
@@ -124,7 +124,7 @@ const testToggleTodo = () => {
     expect(todos(stateBefore, action)).toEqual(stateAfter);
 };
 
-const store = createStore(todos);
+const store = createStore(todoApp);
 console.log('Initial state:');
 console.log(store.getState());
 console.log('--------------');
@@ -153,6 +153,15 @@ console.log('Dispatching TOGGLE_TODO.');
 store.dispatch({
     type: 'TOGGLE_TODO',
     id: 0
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER.');
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
 });
 console.log('Current state:');
 console.log(store.getState());
