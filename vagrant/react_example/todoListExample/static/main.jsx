b0f974a45ef3da58b13665c4459de8bba5d3f6d1
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-02-02 19:04:44 -0900

    Feat: Added todo toggle

--- vagrant/react_example/todoListExample/static/main.jsx
+++ vagrant/react_example/todoListExample/static/main.jsx
@@ -96,8 +96,7 @@ class TodoApp extends React.Component {
             <div>
                 <input ref={node => {
                     this.input = node;
-                }
-                }/>
+                }}/>
                 <button onClick={() => {
                     store.dispatch({
                         type: 'ADD_TODO',
@@ -111,7 +110,16 @@ class TodoApp extends React.Component {
                 <ul>
                     {this.props.todos.map(todo =>
 
-                        <li key={todo.id}>
+                        <li key={todo.id}
+                            onClick={ () => {
+                                store.dispatch({
+                                    type: 'TOGGLE_TODO',
+                                    id: todo.id
+                                });
+                            }}
+                            style={{
+                                textDecoration: todo.completed ? 'line-through' : 'none'
+                            }}>
                             {todo.text}
                         </li>
                     )}
