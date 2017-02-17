7623b6bfa28466034a5fbb02815b435113e8ab68
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-02-16 22:31:12 -0900

    Refactor: Minor style changes to create consistency

--- vagrant/itemCatalog/static/js/modules/main.jsx
+++ vagrant/itemCatalog/static/js/modules/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {provider} from 'react-redux';
-import {content} from './content';
-import configureStore from './store/store';
-import {RestaurantListContainer} from './restaurantList';
+import React from "react";
+import ReactDOM from "react-dom";
+import {provider} from "react-redux";
+import {content} from "./content";
+import configureStore from "./store/store";
 
 const store = configureStore();
 //Todo: http://redux.js.org/docs/basics/UsageWithReact.html#passing-the-store
@@ -15,7 +14,6 @@ const render = () => {
         document.getElementById('reactTest')
     );
 };
-
 
 store.subscribe(render);
 render();
