5c51522a2f3c60bf0541ca7f7e94dbe8d977a939
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-03-22 23:07:31 -0800

    Feat: Extracted combine reducers from main

--- vagrant/react_example/todoListExample/static/reducers/index.js
+++ vagrant/react_example/todoListExample/static/reducers/index.js
@@ -1,4 +1,13 @@
 /**
  * Created by michalfrystacky on 3/22/17.
  */
+import todos from './reducers/todos';
+import visibilityFilter from './reducers/visibilityFilter';
+import { combineReducers } from "redux";
 
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+});
+
+export default todoApp;
